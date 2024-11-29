263f0d66944eebff5ea28ddd30d8e17d9ca60835
Bankole Olaniyi Sunday <adeniyisunday2244@gmail.com>
2024-11-29 05:43:58 +0100

    working on the hero section/ jombotron

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,19 +3,53 @@
 // getting the ham bars and times element
 
 const bars = document.getElementById("ham-bars");
-const times = document.getElementById("ham-times");
+const time = document.getElementById("ham-time");
+const dropdownCtn = document.querySelector(".dropdown-ctn");
+
+console.log(dropdownCtn)
 
 
-bars.addEventListener('click', (e) => {
-    e.preventDefault();
-    bars.classList.add('close'); // Hide the hamburger icon
-    times.classList.remove('close'); // Show the close icon
-});
 
-// Close menu click event
-times.addEventListener('click', (e) => {
-    e.preventDefault();
-    times.classList.add('close'); // Hide the close icon
+bars.addEventListener('click', () => {
+    dropdownCtn.classList.add('open')
+    bars.classList.add('close');
+})
+
+
+time.addEventListener('click', () => {
+    dropdownCtn.classList.remove('open');
     bars.classList.remove('close');
-     // Show the hamburger icon
+})
+
+
+console.log(window.scrollY)
+
+
+
+// this is to listen to the window event for some animation efffect
+
+const hrhead = document.getElementById('hero-heading');
+const hrsub = document.getElementById('hero-subheading');
+const hrbtn = document.getElementById('hero-btn');
+
+// Listen for the scroll event
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY; // Get the vertical scroll position
+
+    // Check if the scroll position reaches a certain point
+    if (scrollPosition > 100) {
+        hrhead.style.transition = 'all 3s ease-in-out';
+        hrhead.style.opacity = '1'; // Ensure the element appears
+    }
+
+    if (scrollPosition > 200) {
+        hrsub.style.transition = 'all 3s ease-in-out';
+        hrsub.style.opacity = '1';
+    }
+
+    if (scrollPosition > 300) {
+        hrbtn.style.transition = 'all 3s ease-in-out';
+        hrbtn.style.opacity = '1';
+    }
 });
+
